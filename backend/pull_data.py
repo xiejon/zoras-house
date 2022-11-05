df3035f70d853d91487f5bf4214cddc4c5d4bd95
Anis C <anis.chihoub@gmail.com>
2022-11-04 22:26:00 -0400

    add files and make new branch

--- backend/pull_data.py
+++ backend/pull_data.py
@@ -3,7 +3,13 @@ conn = sqlite3.connect('database.db', check_same_thread=False)
 cur = conn.cursor()
 
 
+#this function will output all users:
+def pull_all_users():
+    res = cur.execute("SELECT * from USERS")
+    print(res)
 
+
+#pull all groups that a user is in, given access to their user ids. 
 def pull_user_groups(user_id):
     list_of_groups =[]
     res = cur.execute("SELECT group_name, group_id FROM GROUPS WHERE user_id = {}".format(user_id))  
@@ -11,6 +17,7 @@ def pull_user_groups(user_id):
         list_of_groups.append(row)
     return list_of_groups
  
+
 
 def pull_user_chats(user_id):
     list_of_chats =[]
@@ -25,7 +32,7 @@ def pull_user_info(user_id):
     out = res.fetchall()[0]
     return {"id": out[0], "isAdmin": out[1], "location":out[2], "name":out[3], "email":out[4], "tags":out[5], "count_of_event":out[6]}
     
-list_of_chats = pull_user_info(1)
+list_of_chats = pull_all_users()
 print(list_of_chats)
 
 
