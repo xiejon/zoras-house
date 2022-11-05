7638de2985e98aa5409c62e905d38eb6e07cebb9
xiejon <jxie77@gmail.com>
2022-11-05 06:25:21 -0400

    Created group pages
    - Built and styled group pages with components - about, event cal, announcements

--- frontend/src/screens/GroupScreen.js
+++ frontend/src/screens/GroupScreen.js
@@ -1,16 +1,38 @@
 import React from "react";
-import { Container } from "react-bootstrap"
+import { Container, Row, Col, Card } from "react-bootstrap";
 import NavbarComponent from "../components/Navbar";
 import { useNavigate, useParams } from "react-router-dom";
+import CalendarComponent from "../components/CalendarComponent";
+import Announcements from "../components/Announcements";
+import GroupAnnouncements from "../components/GroupAnnouncements";
+import GroupAbout from "../components/GroupAbout";
 
 const GroupScreen = () => {
-    const params = useParams();
-    const { id: groupName } = params;
+  const params = useParams();
+  const { id: groupName } = params;
   return (
-    <Container>
-        <NavbarComponent className="fluid"/>
-        <h1>{groupName}</h1>
-    </Container>
+    <>
+      <NavbarComponent className="fluid" />
+      <Container>
+        <h1 className="mt-2">{groupName}</h1>
+        <Row>
+            <Col className="gap-2">
+            <Card className="w-100 d-flex flex-column justify-content-center align-items-center">
+              <Card.Title className="mt-2">Upcoming Events</Card.Title>
+              <CalendarComponent />
+            </Card>
+            </Col>
+            <Col>
+            <GroupAnnouncements />
+            </Col>
+        </Row>
+        <Row className="mt-2">
+           <Col>
+            <GroupAbout/>
+           </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
