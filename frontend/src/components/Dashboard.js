0c9ff5cc355a82a76d0a681f585a66ccf2ea587a
xiejon <jxie77@gmail.com>
2022-11-04 21:02:47 -0400

    Added progress bar
    - Hard-coded progress bar to track event visits and rewards

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,31 +1,35 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, ProgressBar } from "react-bootstrap";
 import Groups from "./Groups";
 import RecommendedGroups from "./RecommendedGroups";
+import "./Dashboard.css";
 
 const Dashboard = (props) => {
+  const now = 10;
   return (
-    <Container className="h-100vh">
+    <Container className="dashboard-container">
       <Row>
-        <Col className="d-flex flex-column h-100 justify-content-center gap-1">
+        <Col className="mt-3 dashboard-col d-flex flex-column h-100 justify-content-center gap-1">
           <Row>
             <Card className="w-100 d-flex flex-column justify-content-center align-items-center">
-              <Card.Title>Recommended</Card.Title>
+              <Card.Title>Upcoming Events</Card.Title>
+              
             </Card>
           </Row>
           <Row>
             <Card className="w-100 d-flex flex-column justify-content-center align-items-center">
               <Card.Title>Rewards</Card.Title>
+              <ProgressBar className="w-100" now={now} label={`${now} visits`} max="20"/>
             </Card>
           </Row>
         </Col>
 
-        <Col className="d-flex h-100vh justify-content-center gap-1">
-            <RecommendedGroups />
+        <Col className=" mt-3 d-flex h-100vh justify-content-center gap-1">
+          <RecommendedGroups />
         </Col>
 
-        <Col className="d-flex flex-column justify-content-center gap-1">
-        <Row>
+        <Col className=" mt-3 d-flex flex-column justify-content-center gap-1">
+          <Row>
             <Groups />
           </Row>
           <Row>
