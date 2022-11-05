ed5a369c148031f5d5ab42888201d55d1a280ab8
xiejon <jxie77@gmail.com>
2022-11-05 01:34:51 -0400

    Set up login functionality (post request)
    - Added redux reducer to update current user after user logs in
    - Added useNavigate to redirect user after successful login
    - Added post request in preparation for call to database

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,16 +1,48 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+import { setUser } from "../redux/usersSlice";
+import { useNavigate } from "./react-router-dom";
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("", {
+        email,
+        password,
+      });
+      setUser(data);
+      navigate("/home");
+    } catch (err) {
+      alert("Invalid email or password");
+    }
+  };
+
   return (
-    <Form>
+    <Form onSubmit={submitHandler}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          placeholder="Enter email"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
