c394397a9fcf738c23d2818e95765dea5046c8c5
xiejon <jxie77@gmail.com>
2022-11-04 22:25:17 -0400

    Added API call to backend
    - Created successful get call to receive data from flask backend

--- frontend/src/components/Groups.js
+++ frontend/src/components/Groups.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GroupItem from "./GroupItem";
+import axios from "axios";
 import { Card } from "react-bootstrap";
 
 const sample = [
@@ -21,14 +22,29 @@ const sample = [
 ];
 
 const Groups = () => {
+  const [groups, setGroups] = [];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get("http://127.0.0.1:5000/data");
+      return data.data;
+    };
+
+    const result = fetchData()
+    .catch((err) => console.log(err.message));
+   
+  }, []);
+
   return (
     <Card className="w-100 d-flex flex-column justify-content-center align-items-center">
-      <Card.Title className="d-flex align-items-center justify-content-center">Groups</Card.Title>
+      <Card.Title className="d-flex align-items-center justify-content-center">
+        Groups
+      </Card.Title>
       <Card.Body className="w-100 d-flex flex-column gap-1">
         {sample.map((group) => {
           return (
-            <GroupItem 
-            key={group.group_id}
+            <GroupItem
+              key={group.group_id}
               userId={group.user_id}
               groupId={group.group_id}
               groupName={group.group_name}
